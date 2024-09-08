f847bdeaf3fe0337381ec5ef6bc0695f4cbb532c
SebastianCamposI <105455746+SebastianCamposI@users.noreply.github.com>
2024-09-08 19:44:54 +0000

    Home page

--- web.py
+++ web.py
@@ -1,31 +1,39 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import parse_qsl, urlparse
-
+import os
 
 class WebRequestHandler(BaseHTTPRequestHandler):
-    def url(self):
-        return urlparse(self.path)
-
-    def query_data(self):
-        return dict(parse_qsl(self.url().query))
-
     def do_GET(self):
-        self.send_response(200)
+        if self.path == '/':
+            self.handle_home()
+        else:
+            self.handle_404()
+
+    def handle_home(self):
+        try:
+            # Lee el contenido del archivo home.html
+            with open('home.html', 'r') as file:
+                content = file.read()
+            
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html")
+            self.end_headers()
+            self.wfile.write(content.encode("utf-8"))
+        except FileNotFoundError:
+            self.handle_404()
+
+    def handle_404(self):
+        self.send_response(404)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
-
-    def get_response(self):
-        path_parts = self.url().path.strip('/').split('/')
-        project = path_parts[1] if len(path_parts) > 1 else 'desconocido'
-        web_part = path_parts[2] if len(path_parts) > 2 else 'desconocido'
-        
-        author = self.query_data().get('autor', 'desconocido')
-
-        return f"""
-        <h1>Proyecto: {web_part} Autor: {author}</h1>
-        """
-
+        self.wfile.write(b"""
+        <html>
+        <head><title>404 Not Found</title></head>
+        <body>
+        <h1>404 Not Found</h1>
+        <p>The requested URL was not found on this server.</p>
+        </body>
+        </html>
+        """)
 
 if __name__ == "__main__":
     print("Starting server")
